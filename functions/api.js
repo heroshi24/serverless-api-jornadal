e21e887fa9ae611bc60369b46fd17b3d0a253927
heroshi24 <heroshi66@gmail.com>
2024-06-06 18:08:35 +0000

    finish

--- functions/api.js
+++ functions/api.js
@@ -1,23 +1,23 @@
 const express = require('express');
 const serverless = require('serverless-http');
-const router = require('./routes/author');
+const router = require('./routes/recipeRoutes'); // Change to recipeRoutes
 const mongoose = require('mongoose');
 const cors = require('cors');
 
-const app= express();
+const app = express();
 
-const dbCloudUrl ='mongodb+srv://jornadaljarnalsin:C9991CA4DCC3@cluster0.rvkhrov.mongodb.net/JornadalDB';
+const dbCloudUrl = 'mongodb+srv://jornadaljarnalsin:C9991CA4DCC3@cluster0.rvkhrov.mongodb.net/JornadalDB';
 
-const dbLocalUrl = 'mongodb://localhost:27017/express-mongo-api';
+const dbLocalUrl = 'mongodb://localhost:27017/recipe-db'; // Change to your recipe database name
 
 app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
 mongoose
-.connect(dbCloudUrl || dbLocalUrl)
-.then(() => console.log('Connected to MongoDB'))
-.catch((error) => console.error('Failed to connect to mongoDB', error));
+    .connect(dbCloudUrl || dbLocalUrl, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('Connected to MongoDB'))
+    .catch((error) => console.error('Failed to connect to MongoDB', error));
 
-app.use('/.netlify/functions/api',router);
+app.use('/.netlify/functions/api', router);
 module.exports.handler = serverless(app);
